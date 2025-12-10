fbc0a5a1d18d0ca301194d4dd938df770f78e268
Dhruv Narula <dhruvmn27@gmail.com>
2025-12-10 09:43:45 -0800

    Update fonts.py to add set_plot_style()

--- src/fonts.py
+++ src/fonts.py
@@ -3,8 +3,37 @@ import matplotlib.pyplot as plt
 import os
 
 def register_font(font_path: str):
-    """Registers a custom TTF font for Matplotlib"""
+    # Register custom font
     fm.fontManager.addfont(font_path)
     font_name = fm.FontProperties(fname=font_path).get_name()
     plt.rcParams['font.family'] = font_name
     return font_name
+
+def set_plot_style():
+  # Register font files - Montserrat
+  font_paths = [
+      "/content/football-ds-learning/fonts/Montserrat-Regular.ttf",
+      "/content/football-ds-learning/fonts/Montserrat-Medium.ttf",
+      "/content/football-ds-learning/fonts/Montserrat-SemiBold.ttf",
+      "/content/football-ds-learning/fonts/Montserrat-Bold.ttf",
+      "/content/football-ds-learning/fonts/Montserrat-Light.ttf",
+  ]
+
+  for path in font_paths:
+    register_font(path)
+
+  # Set defaults
+  plt.rcParams["font.family"] = 'Montserrat'
+  plt.rcParams["font.weight"] = 'regular'
+  plt.rcParams["figure.facecolor"] = "#f2f2f2"
+
+  # Specific styles
+  title_font = FontProperties(family="Montserrat", weight="semibold", size=44)
+  subtitle_font = FontProperties(family="Montserrat", weight="medium", size=22)
+  dot_label_font = FontProperties(family="Montserrat", weight="medium", size=16)
+  axis_font = FontProperties(family="Montserrat", weight="medium", size=28)
+  label_font = FontProperties(family="Montserrat", weight="medium", size=20)
+  tick_font = FontProperties(family="Montserrat", weight="regular", size=16)
+  footer_font = FontProperties(family="Montserrat", weight="regular", size=14)
+
+  return title_font, subtitle_font, dot_label_font, axis_font, label_font, tick_font, footer_font
